c0b6a44648b0bf63ee093e469561e13513844cd6
bianjianhuang <bianjianhuang@huya.com>
2023-04-13 15:52:19 +0800

    add ieltsonlinetests and sixstone spider

--- topic/source/chatgpt.go
+++ topic/source/chatgpt.go
@@ -1,1 +1,11 @@
 package source
+
+import (
+	"fmt"
+)
+
+type ChatGPT struct{}
+
+func (robot *ChatGPT) Run() {
+	fmt.Printf("robot: %v\n", robot)
+}
--- topic/types.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "time"
-
-type Topic struct {
-	Content    string
-	CreateTime *time.Time
-	Source     string
-}
